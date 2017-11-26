0a0acaf7895817e26b9228613f8ab7b66420a8f0
Lindsey Trainor <ltrainor38@gmail.com>
2017-11-26 01:05:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelectorAll('#nested')[0]
+}
+
+function deepestChild(){
+  
+  var node = document.querySelector('#grand-node')
+  
+  for (var i = 0; i < 4; i++){
+    node = node.querySelector('div')
+  }
+ return node
+}
+
+function increaseRankBy(n){
+  var a = document.getElementByClass()
+}
